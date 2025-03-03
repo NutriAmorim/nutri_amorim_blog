bcaad33c8dd272fe3be24282e21cda983b079dbf
Marcos F. Amorim <nutriamorim69@gmail.com>
2025-03-03 11:51:31 -0300

    Adicionando novas funcionalidades ao blog

--- meu_app/models.py
+++ meu_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Post(models.Model):
+    title = models.CharField(max_length=100, default='Nutri Amorim')  # Título do post
+    content = models.TextField()  # Conteúdo do post (pode ser o texto ou descrição)
+    created_at = models.DateTimeField(auto_now_add=True)  # Data de criação do post
+    updated_at = models.DateTimeField(auto_now=True)  # Data da última atualização
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+
--- models.py
+++ models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Postagem(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.titulo
